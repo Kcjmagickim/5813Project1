7bedcc6e8c36faec9fd31b344d08e09801388de9
John Kim <Kcjmagickim@gmail.com>
2018-02-03 20:15:00 -0700

    .c and .h files

---
+++ project1-updated/memory .h
@@ -0,0 +1,18 @@
+#ifndef MEMORY_H_
+#define MEMORY_H_
+/*Header file for memeory manipulation functions*/
+
+uint8_t *my_memmove(uint8_t *src, uint8_t *dst, size_t length);
+
+uint8_t *my_memcpy(uint8_t *src, uint8_t *dst, size_t length);
+
+uint8_t *my_memset(uint8_t *src, size_t length, uint8_t value);
+
+uint8_t *my_memzero(uint8_t *src, size_t length);
+
+uint8_t *my_reverse(uint8_t *src, size_t length);
+
+uint8_t *reverse_words(size_t length);
+
+uint8_t *free_words(uint32_t *src);
+#endif
